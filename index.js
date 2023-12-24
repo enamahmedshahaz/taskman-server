5cbecb48e1e80eab90e6e4dc9071e0a14a3e151a
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-12-25 00:05:38 +0600

    add user related api

--- index.js
+++ index.js
@@ -16,31 +16,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        //  await client.connect();
 
-    const database = client.db("TaskManDB");
-    const userCollection = database.collection("users");
-    const taskCollection = database.collection("tasks");
+        const database = client.db("TaskManDB");
+        const userCollection = database.collection("users");
+        const taskCollection = database.collection("tasks");
+
+        // API to insert users data
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null });
+            }
+            
+            const result = await userCollection.insertOne({ ...user, createdAt: new Date() });
+
+            res.send(result);
+        });
+
+        //API to get a user info based on email queryParam
+        app.get('/users', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
 
 
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
